a6c8ccbea4b4a2a114e3b7e3fc6204ddb69a9163
yuangs <516309388@qq.com>
2016-07-19 11:05:06 +0800

    Update code.py

--- main/code.py
+++ main/code.py
@@ -6,33 +6,34 @@ Created on Fri Jun 03 14:41:30 2016
 @Email: yuanguangshan@163.com
 
 本模块实现如下功能：
-1、由用户通过键盘输入edb指标的id（不带前缀s),可输入任意多个edb指标，提取数据到指定的
-csv文件；
-2、用户也可用excel一次性导出多个edb指标的id，并复制到剪切贴上，模块会自动提取代码并
-取数据；
-3、输入截止日期由用户输入，如果不输入，采用默认值;
+1. oneCode:获取客户输入的单一windcode
+2. windCode:一次性获得客户输入的多个windcode;
+3. getCodeFromClipboard:通过剪切板获得用户复制到剪切板上的wind代码；
+4. getCodes：利用正则表达式获取客户剪切板中的多个代码；
+5.  getWindCodes：利用正则表达式获取客户剪切板中的多个代码（wind品种指数；
+6. getClipboard：获取剪切板数据；
+7. setClipboard：设置剪切板数据；
+8. getActivecodes：从具体合约代码获取主力合约代码
 
 """
 
 import re
-
 #import win32clipboard as wc
 #import win32con
-
 from WindPy import *
 from pandas import Series, DataFrame
 
 w.start()  # 启动wind接口
 
 def oneCode():
-    '获取客户输入的单一windcode'
+    '获取用户输入的单一windcode，如cu.shf(行情）,S15458785(EDB)'
 
     code = input('Please input a code:')
     return code
 
 
 def windCode():
-    '逐一获取客户输入的edb代码，并合为一个字符串，用于通过wind python接口提取数据'
+    '逐一获取用户输入的edb代码，并合为一个字符串，用于通过wind python接口提取数据'
 
     codes = []
     c = 's5707135'
@@ -40,8 +41,6 @@ def windCode():
     while True:
         edb_code = input(r"EDB code:")
         if edb_code != "":
-            # edb_code='s'+
-            # edb_code,有些指标s或m都有指标（m0175593，sm0175593)，容易混淆，所以输入全部，不能只输入数字
             codes.append(edb_code)
         else:
             break
@@ -50,18 +49,14 @@ def windCode():
 
 
 def getCodeFromClipboard():
-    '''
-    通过剪切板获得用户复制到剪切板上的edb代码，利用这些代码来调用接品获取数据
-    '''
+    '通过剪切板获得用户复制到剪切板上的edb代码，利用这些代码来调用接口获取数据'
     d = getClipboard()
-#  wc.OpenClipboard()
-#  d = wc.GetClipboardData(win32con.CF_TEXT)
-#  wc.CloseClipboard()
     codes = re.findall('[SM]+\d{7}', str(d))
     return codes
 
 
 def getClipboard():
+    '获取剪切板中的数据'
     import win32clipboard as w
     import win32con
     w.OpenClipboard()
@@ -71,6 +66,7 @@ def getClipboard():
 
 
 def setClipboard(aString):
+     '设置剪切板中的数据'
     import win32clipboard as w
     import win32con
     w.OpenClipboard()
@@ -89,14 +85,14 @@ def getCodes():
 
 
 def getWindCodes():
-    '利用正则表达式获取客户剪切板中的多个代码（wind品种指数）'
+    '利用正则表达式获取客户剪切板中的多个代码（wind商品品种指数，如RUFI.WI）'
     d = getClipboard()
     codes = re.findall('[A-Za-z]{3,4}\.WI', str(d))
     return codes
 
 
 def getActivecodes():
-    '将代码去除数字后返回，如ru1609.shf返回ru.shf'
+    '具体期货合约代取获取wind主力合约代码，如ru1609.shf返回ru.shf'
     rawcodes = str(getCodes())
     s = re.sub('\d{3,4}', '', rawcodes)
     codes = setClipboard(s)
