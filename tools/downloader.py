f0e43e1c1d200f3d62840a83470f9e0ec4cbfcfa
gsyuan <yuanguangshan@163.com>
2016-08-25 17:28:33 +0800

    完善下载文件的命名

--- tools/downloader.py
+++ tools/downloader.py
@@ -9,15 +9,13 @@ Created on Thu Aug 11 14:11:01 2016
 import os,re
 import requests
 from lxml import etree
-#测试下载链接，要下载的网页链接，获取网页标题
-#格隆汇 http://www.gelonghui.com/p/84066.html
-# 财新网 http://finance.caixin.com/2016-08-24/100981248.html
-# 华尔街见闻 http://wallstreetcn.com/node/259499
-# 雪球 https://xueqiu.com/today/all
-# 知乎 https://www.zhihu.com/question/27434092
-# 中期协 http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519469842030.doc
-# http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519470578820.xls
+
 def save(url):
+    """
+    给出一个网址，保存网页或文件
+    :param url:
+    :return: 网页标题或者文件名
+    """
     url = url
     curr_dir = os.getcwd()
     headers = {
@@ -63,13 +61,39 @@ def save(url):
     download()
 
 if __name__=='__main__':
-    urls=['http://pmi.caixin.com/2016-08-24/100981097.html',
-        'http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519469842030.doc',
-        'http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519470578820.xls'
-    ]
-    for url in urls:
-        try:
-            save(url)
-        except:
-            pass
+    # 测试下载链接，要下载的网页链接，获取网页标题
+    # 格隆汇 http://www.gelonghui.com/p/84066.html
+    # 财新网 http://finance.caixin.com/2016-08-24/100981248.html
+    # 华尔街见闻 http://wallstreetcn.com/node/259499
+    # 雪球 https://xueqiu.com/today/all
+    # 知乎 https://www.zhihu.com/question/27434092
+    # 中期协 http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519469842030.doc
+    # http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519470578820.xls
+    def download_htmls():
+        python=open(r"C:\Users\gsyuan\Desktop\url.txt",'r').readlines()
+        python = [item.strip() for item in python]
+        for url in python:
+            try:
+                save(url)
+            except:
+                pass
+
+    def test():
+        urls = ['http://pmi.caixin.com/2016-08-24/100981097.html',
+                'http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519469842030.doc',
+                'http://www.cfachina.org/CXFW/zgsyw/ywgzzgs/201512/P020160104519470578820.xls']
+
+        for url in urls:
+            try:
+                save(url)
+            except:
+                pass
+    test()
+    download_htmls()
+
+
+
+
+
+
 
