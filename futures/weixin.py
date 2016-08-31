4719dac3c30aa7e007445d4ef7edfb2d55458bc1
gsyuan <yuanguangshan@163.com>
2016-08-31 14:46:32 +0800

    xueqiu

--- futures/weixin.py
+++ futures/weixin.py
@@ -1,9 +1,13 @@
 
-import downloader
+import downloader,clip
 import time
 import requests
+import re
 
-
+pattern='<h4 class="msg_title" hrefs="(.*?)">'
+content=clip.getClipboard()
+urls=re.findall(pattern,content)
+print(urls)
 
 def weixin():
     weixin_url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzA4NDg3MDEyNw==&uin=Mzc2NDQxNjgw&key=cf237d7ae24775e875012df81a1d85f398c8b82d22dcce2566b94c7eed5fa702ce897fb2533b33d0183c5a012b0b4de69fda6646b31cb3ee&devicetype=android-23&version=26031932&lang=zh_CN&nettype=ctnet&pass_ticket=u2SNofNIhhIn5ecF5KAsG1rq%2F8U2I62vX3h%2FZB94bWWUvl0CE6uyD81sEtcthctb&wx_header=1#wechat_webview_type=1"
