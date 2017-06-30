ebbbb63aa99105a9c2085d1af32b332f30ce9292
yuangs <516309388@qq.com>
2017-06-30 15:19:43 +0800

    修正数据获取逻辑
    flag=0，获得非主力合约；
    flag=1，获得主力合约；
    flag=-1，获得全部合约

--- futures/future_quote.py
+++ futures/future_quote.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import re
 import json
 import requests
 
@@ -25,39 +23,29 @@ def future_quote_sina(exchange='all', flag=1):
     # 全部主力合约：df[(df.market=='dce')& (df.is_hot=='1')]
     # 交易所合约：df[(df.market=='dce')]
     if exchange == 'all':
-        if flag != 2:
-            # 三大交易所主力（非主力）合约
+        if flag == 0:
+            # flag=0,返回四大交易所非主力合约
             return df[df.is_hot == flag].sort_values('volume', ascending=False)
-        else:
-            # 三大交易所全部合约
+        elif flag==1:
+            #flag=1,返回四大交易所主力合约
+            return df[df.is_hot >= flag].sort_values('volume', ascending=False)
+        elif flag==-1:
+            # flag=-1,返回四大交易所全部合约
             return df.sort_values('volume', ascending=False)
     else:
-        if flag != 2:
             # 指定交易所主力（非主力）合约
+        if flag == 0:
+            #返回指定交易所非主力合约
             return df[(df.market == exchange) & (df.is_hot == flag)].sort_values('volume', ascending=False)
+        elif flag == 1:
+            #返回指定交易所非主力合约
+            return df[(df.market == exchange) & (df.is_hot >= flag)].sort_values('volume', ascending=False)
         else:
             # 指定交易所全部合约
             return df[df.market == exchange].sort_values('volume', ascending=False)
 
-def future_overseas_sina():
-    '获取新浪期货外盘期货的即时行情数据'
-    r = requests.get('http://vip.stock.finance.sina.com.cn/d/api/openapi_proxy.php/?__s=[[%22wpqh%22,%22spqh%22]]')
-    data = r.text
-    data = json.loads(data)[0]
-    df=pd.DataFrame(data['items'],columns=data['fields'])
-    df.index=df.symbol
-    columns=['name','dateupdate','timeupdate', 'last', 'changeamount', 'pricechange','totalvol',\
-         'bid', 'ask','prev', 'open', 'high', 'low']
-    data=df.loc[:,columns]
-    return data
-
 
 if __name__=='__main__':
-    import time
-    while True:
-        a=future_quote_sina('all', 1)
-        print(a.loc['铁矿石1705',['name', 'trade','volume', 'position']])
-        #print(a.columns)
-        print('小猪猪，数据提取完毕!!!!')
-        time.sleep(1)
-    
+    f=future_quote_sina('czce',-1)
+    print(f.shape)
+    print(f.name)
