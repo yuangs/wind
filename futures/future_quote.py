f0496d3ef9c53afdbab8c441d2e2d5316a4204b9
yuangs <516309388@qq.com>
2017-07-27 17:06:40 +0800

    Update future_quote.py

--- futures/future_quote.py
+++ futures/future_quote.py
@@ -43,8 +43,29 @@ def future_quote_sina(exchange='all', flag=1):
         else:
             # 指定交易所全部合约
             return df[df.market == exchange].sort_values('volume', ascending=False)
-
-
+        
+#从上期所网站批量下载日成交数据后存入数据库
+def price_to_sql(start='20170523'):
+    import sqlite3
+    con=sqlite3.connect(r'd:\aliyun\www\my_flask\marketPrice\FutureMarketPrice.db')
+    df=shfe_price(start)
+    for i in pd.bdate_range(start=start,periods=50):
+        date=i.strftime('%Y%m%d')
+        print(date)
+        try:
+            data=shfe_price(date=date)
+            df=pd.concat([data,df])
+        except:
+            print('Error occured!%s'%date)
+    CODE=(df.PRODUCTID.str.upper().str.strip().str[:-2]+df.DELIVERYMONTH)
+    df['code']=CODE
+    df=df.loc[:,['code','date', 'OPENPRICE','HIGHESTPRICE','LOWESTPRICE','CLOSEPRICE','SETTLEMENTPRICE','VOLUME','OPENINTEREST']].copy()
+    df.columns=['code','date', 'open','high','low','close','settle','vol','oi']
+    df=df[df.settle!=""]
+    df.to_sql('FutureMarketPrice',con,if_exists='append',index=False)
+    con.close()
+    return df
+    
 if __name__=='__main__':
     f=future_quote_sina('czce',-1)
     print(f.shape)
