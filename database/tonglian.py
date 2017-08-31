6ec8f965d8b696c9f7c977edaf6428671d79a823
yuangs <yuanguangshan@163.com>
2017-08-31 14:49:42 +0800

    增加通联数据接口

---
+++ database/tonglian.py
@@ -0,0 +1,14 @@
+https://gw.wmcloud.com/rrp/mobile/whitelist/data?indicID=2090102413&beginDate=20100101&endDate=20171231
+https://gw.wmcloud.com/rrp/mobile/whitelist/data?indicID=2090102430&beginDate=20100101&endDate=20171231
+https://gw.wmcloud.com/rrp/mobile/whitelist/data?indicID=1020001857&beginDate=20100101&endDate=20171231
+             产量:冰柜  产量:冰柜:同比    产量:轿车
+date                                 
+2010-12-31     NaN       NaN   957.59
+2011-12-31     NaN       NaN  1012.67
+2012-12-31  1628.2      -2.8  1077.00
+2013-12-31  1803.0      10.7  1210.43
+2014-12-31  1947.7       8.0  1248.31
+2015-12-31  2025.0       6.1  1170.00
+2016-12-31  1199.6       NaN  1223.80
+
+***Repl Closed***
