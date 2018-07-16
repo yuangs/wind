f50cd7fe3fd78c58399e51239454c50eb50e27a8
yuangs <516309388@qq.com>
2018-07-16 18:01:34 +0800

    Update get_99qh_oi.py

--- database/get_99qh_oi.py
+++ database/get_99qh_oi.py
@@ -97,17 +97,76 @@ def get_position_contract(date="2018-07-10",goods='cu'):
 #     print(url)
     return re.findall('<AgreementCode>(.*?)</AgreementCode>',df.text)
 
+
 def get_oi(date="2018-07-10",goods=6,contract='cu1809',count=20):
     url=r'http://service.99qh.com/hold2/MemberHold/GetTableHtml.aspx?\
 date=%s&user=99qh&goods=%s&agreement=%s&count=%s'%(date,goods,contract,count)
-    print(url)
+#     print(url)
     df=pd.read_html(url)
-    data=pd.concat([df[0],df[2],df[3]]).iloc[2:,1:]
-    print(data)
+    data=pd.concat([df[0],df[2],df[3]]).iloc[2:,]
+#     print(data)
     return data
+    
 
-for i in var_list:
-    con_list=get_position_contract(goods=i)
-    for j in con_list:
-        data=get_oi(date="2018-07-10",goods=dict_99['99qh品种代码'][i],contract=j,count=20)
-        print(data)
+def getData(df=data):
+    df=data[0][2:]
+    df['v']=df.iloc[1,2]
+    df['l']=df.iloc[1,6]
+    df['s']=df.iloc[1,10]
+    df['vr']=df.iloc[0,0]
+    df['lr']=df.iloc[0,1]
+    df['sr']=df.iloc[0,2]
+    a=df[2:]
+    va=pd.DataFrame(a.loc[:,[0,1,2,3,'v','vr']])
+    la=pd.DataFrame(a.loc[:,[4,5,6,7,'l','lr']])
+    sa=pd.DataFrame(a.loc[:,[8,9,10,11,'s','sr']])
+    va.columns=la.columns=sa.columns=['Rank','Member','Value','Change','Index','Rank_type']
+    aa=pd.concat([va,la,sa])
+    df=data[2]
+    df['v']=df.iloc[1,2]
+    df['l']=df.iloc[1,6]
+    df['s']=df.iloc[1,10]
+    df['vr']=df.iloc[0,0]
+    df['lr']=df.iloc[0,1]
+    df['sr']=df.iloc[0,2]
+    b=df[2:]
+    vb=pd.DataFrame(b.loc[:,[0,1,2,3,'v','vr']])
+    lb=pd.DataFrame(b.loc[:,[4,5,6,7,'l','lr']])
+    sb=pd.DataFrame(b.loc[:,[8,9,10,11,'s','sr']])
+    vb.columns=lb.columns=sb.columns=['Rank','Member','Value','Change','Index','Rank_type']
+    bb=pd.concat([vb,lb,sb])
+    df=data[3]
+    df['v']=df.iloc[1,2]
+    df['l']=df.iloc[1,6]
+    df['s']=df.iloc[1,10]
+    df['vr']=df.iloc[0,0]
+    df['lr']=df.iloc[0,1]
+    df['sr']=df.iloc[0,2]
+    c=df[2:]
+    vc=pd.DataFrame(c.loc[:,[0,1,2,3,'v','vr']])
+    lc=pd.DataFrame(c.loc[:,[4,5,6,7,'l','lr']])
+    sc=pd.DataFrame(c.loc[:,[8,9,10,11,'s','sr']])
+    vc.columns=lc.columns=sc.columns=['Rank','Member','Value','Change','Index','Rank_type']
+    cc=pd.concat([vc,lc,sc])
+    dd=pd.concat([aa,bb,cc],ignore_index=True)
+    return dd
+
+def get_OIData(date="2018-07-02"):
+    Data=pd.DataFrame()
+    for i in var_list:
+        contract_list=get_position_contract(date,i)
+        goods=dict_99['99qh品种代码'][i]
+        Dataj=pd.DataFrame()
+        for j in contract_list:
+            data=get_oi(date="2018-07-03",goods=goods,contract=j,count=20)
+            data=getData(data)
+            data['Date']=date
+            data['Commodity']=i
+            data['Contract']=j
+            Dataj=pd.concat([Dataj,data],ignore_index=True)
+        Data=pd.concat([Data,Dataj],ignore_index=True)
+        Data.to_csv('Daily_oi.csv')
+    return Data
+
+if __name__ == '__main__':
+    get_OIData()
